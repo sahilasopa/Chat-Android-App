9f125466cad5659c9c387044d4b60d36ad62f429
sahilasopa <sahilasopa12@gmail.com>
2021-07-26 20:07:50 +0530

    Commit

--- app/src/main/java/com/sahilasopa/auth/MainActivity.java
+++ app/src/main/java/com/sahilasopa/auth/MainActivity.java
@@ -2,7 +2,6 @@ package com.sahilasopa.auth;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -10,22 +9,30 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.sahilasopa.auth.adapter.UserAdapter;
 import com.sahilasopa.auth.databinding.ActivityMainBinding;
+import com.sahilasopa.auth.models.Users;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     FirebaseAuth auth;
     DatabaseReference databaseReference;
-    ArrayList<String> user_list;
+    private RecyclerView recyclerView;
+    private UserAdapter userAdapter;
+    private List<Users> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,29 +41,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
         auth = FirebaseAuth.getInstance();
         databaseReference = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference users = databaseReference.child("Users");
-        user_list = new ArrayList<>();
-        ValueEventListener valueEventListener = new ValueEventListener() {
+        if (auth.getCurrentUser() == null) {
+            Intent intent = new Intent(this, login.class);
+            startActivity(intent);
+        }
+        recyclerView = binding.recyclerView;
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        users = new ArrayList<>();
+        readUsers();
+    }
+
+    private void readUsers() {
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
                 for (DataSnapshot ds : snapshot.getChildren()) {
-                    String name = ds.child("id").getValue(String.class);
-                    Log.d("TAG", name);
-                    user_list.add(name);
+                    Users user = ds.getValue(Users.class);
+                    assert user != null;
+                    assert firebaseUser != null;
+                    if (!(user.getId().equals(firebaseUser.getUid()))) {
+                        users.add(user);
+                    }
                 }
+                userAdapter = new UserAdapter(getApplicationContext(), users);
+                recyclerView.setClipToPadding(false);
+                recyclerView.setAdapter(userAdapter);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
-        };
-        users.addValueEventListener(valueEventListener);
-        Log.v("response", String.valueOf(auth.getCurrentUser()));
-        if (auth.getCurrentUser() == null) {
-            Intent intent = new Intent(this, login.class);
-            startActivity(intent);
-        }
+        });
     }
 
     @Override
@@ -68,15 +88,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.settings:
-                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.logout:
-                Intent intent = new Intent(this, login.class);
-                auth.signOut();
-                startActivity(intent);
-                break;
+        if (item.getItemId() == R.id.settings) {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+        } else if (item.getItemId() == R.id.logout) {
+            Intent intent = new Intent(this, login.class);
+            auth.signOut();
+            startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
     }
---
+++ app/src/main/java/com/sahilasopa/auth/adapter/UserAdapter.java
@@ -0,0 +1,65 @@
+package com.sahilasopa.auth.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sahilasopa.auth.R;
+import com.sahilasopa.auth.models.Users;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    private final Context context;
+    private final List<Users> users;
+
+    public UserAdapter(Context context, List<Users> users) {
+        this.context = context;
+        this.users = users;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull UserAdapter.ViewHolder holder, int position) {
+        Users user = users.get(position);
+        holder.textView.setText(user.getUsername());
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        public TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textView);
+            ConstraintLayout cardView = itemView.findViewById(R.id.userView);
+            textView.setOnClickListener(this);
+            cardView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int pos = getAdapterPosition();
+            Users user = users.get(pos);
+            Toast.makeText(context, user.getUsername(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
